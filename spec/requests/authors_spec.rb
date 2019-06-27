cee9b06551ef5109f2eb0001af19cbd86e475c0b
SRSSavage <zach2487@gmail.com>
2019-06-27 16:01:59 -0600

    changed spec/authors to author

--- spec/requests/authors_spec.rb
+++ spec/requests/authors_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Author", type: :request do
   describe "GET /authors" do
     it "works! (now write some real specs)" do
-      get new_authors_path
+      get new_author_path
       expect(response).to have_http_status(200)
     end
   end
